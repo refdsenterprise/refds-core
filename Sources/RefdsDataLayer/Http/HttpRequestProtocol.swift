908ef7b47345bd63df30d698773af111c7b7e31a
Rafael Escaleira <rafael.escaleira@deliverit.com.br>
2022-09-24 00:10:09 -0400

    Add client on http request

--- Sources/RefdsDataLayer/Http/HttpRequestProtocol.swift
+++ Sources/RefdsDataLayer/Http/HttpRequestProtocol.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public protocol HttpRequestProtocol: HttpEndpointProtocol {
     associatedtype Response
+    var httpClient: HttpClient { get set }
     func decode(_ data: Data) throws -> Response
 }
 
