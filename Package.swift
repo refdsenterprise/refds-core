cc97f6f1a8b703e5f571a44273ae5aae9455293d
Rafael Escaleira <rafael.escaleira@deliverit.com.br>
2022-09-28 02:52:04 -0400

    Change project name

--- Package.swift
+++ Package.swift
@@ -4,21 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "RefdsCleanArchitecture",
+    name: "RefdsCore",
     platforms: [
         .iOS(.v13),
         .macOS(.v12),
         .macCatalyst(.v15)
     ],
     products: [
-        .library(name: "RefdsDomainLayer", targets: ["RefdsDomainLayer"]),
-        .library(name: "RefdsDataLayer", targets: ["RefdsDataLayer"]),
-        .library(name: "RefdsInfraLayer", targets: ["RefdsInfraLayer"])
+        .library(
+            name: "RefdsCore",
+            targets: ["RefdsDomain", "RefdsData", "RefdsInfra"])
     ],
     dependencies: [],
     targets: [
-        .target(name: "RefdsDomainLayer", dependencies: []),
-        .target(name: "RefdsDataLayer", dependencies: ["RefdsDomainLayer"]),
-        .target(name: "RefdsInfraLayer", dependencies: ["RefdsDataLayer", "RefdsDomainLayer"])
+        .target(
+            name: "RefdsDomain",
+            dependencies: []),
+        .target(
+            name: "RefdsData",
+            dependencies: ["RefdsDomain"]),
+        .target(
+            name: "RefdsInfra",
+            dependencies: ["RefdsData", "RefdsDomain"])
     ]
 )
