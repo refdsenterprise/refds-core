ee48cc1c76c24b3c9e063068e804f75e589e77eb
Rafael Escaleira <rafael.escaleira@deliverit.com.br>
2022-09-28 15:02:14 -0400

    Continuos receive websocket network

--- Sources/RefdsInfra/WebSocket/WebSocketNetworkAdapter.swift
+++ Sources/RefdsInfra/WebSocket/WebSocketNetworkAdapter.swift
@@ -79,6 +79,7 @@ public class WebSocketNetworkAdapter: NSObject, WebSocketClient {
                 }
                 self.receiveSemaphore.signal()
             }
+            didReceive()
         })
     }
     
