ab87abf1fbc3ccfff56a9adf4cba394d16354630
Rafael Escaleira <rafael.escaleira@g.globo>
2023-04-13 12:20:55 -0400

    Add timestamp attribute on date

--- Sources/RefdsCore/Domain/Helpers/Date+Extensions.swift
+++ Sources/RefdsCore/Domain/Helpers/Date+Extensions.swift
@@ -3,17 +3,23 @@ import Foundation
 public extension Date {
     static var current: Date { return Date() }
     
+    var timestamp: TimeInterval { self.timeIntervalSince1970 }
+    
     func asString(withDateFormat dateFormat: String.DateFormat = .dayMonthYear) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "pt_BR")
         dateFormatter.dateFormat = dateFormat.value
         return dateFormatter.string(from: self)
     }
+}
+
+public extension TimeInterval {
+    var date: Date { Date(timeIntervalSince1970: self) }
     
-    func formatted(with dateFormat: String.DateFormat = .dayMonthYear) -> Date {
+    func asString(withDateFormat dateFormat: String.DateFormat = .dayMonthYear) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "pt_BR")
         dateFormatter.dateFormat = dateFormat.value
-        return dateFormatter.date(from: self.asString(withDateFormat: dateFormat)) ?? .current
+        return dateFormatter.string(from: self.date)
     }
 }
