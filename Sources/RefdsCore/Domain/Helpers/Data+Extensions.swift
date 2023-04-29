e4d286ffa913f0d7992f7339c805ef311ce0ddde
Rafael Escaleira <rafaelescaleira@icloud.com>
2023-04-29 16:11:56 -0400

    Add date format on decode json

--- Sources/RefdsCore/Domain/Helpers/Data+Extensions.swift
+++ Sources/RefdsCore/Domain/Helpers/Data+Extensions.swift
@@ -1,7 +1,15 @@
 import Foundation
 
 public extension Data {
-    func asModel<T: Decodable>() -> T? {
-        return try? JSONDecoder().decode(T.self, from: self)
+    func asModel<T: Decodable>(dateFormat: String? = nil) -> T? {
+        let jsonDecoder = JSONDecoder()
+        
+        if let dateFormat = dateFormat {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = dateFormat
+            jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        }
+        
+        return try? jsonDecoder.decode(T.self, from: self)
     }
 }
