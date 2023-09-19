fa6de1684d42d4e250a2a5227d352c90440be04c
Rafael Escaleira <rafaelescaleira@icloud.com>
2023-09-19 17:17:29 -0400

    Update DomainError.swift

--- Sources/RefdsCore/Domain/DomainError.swift
+++ Sources/RefdsCore/Domain/DomainError.swift
@@ -4,12 +4,14 @@ public enum DomainError: Error, CustomStringConvertible {
     case decodedError(type: Decodable.Type)
     case encodedError(type: Encodable.Type)
     case requestError(error: Error)
+    case notFound(type: Any.Type)
     
     public var description: String {
         switch self {
         case .decodedError(let decodedType): return "Error on decoded - \(decodedType.self)"
         case .encodedError(let encodedType): return "Error on encoded - \(encodedType.self)"
         case .requestError(let description): return "Error on request - \(description)"
+        case .notFound(let type): return "Error - not found - \(type.self)"
         }
     }
 }
