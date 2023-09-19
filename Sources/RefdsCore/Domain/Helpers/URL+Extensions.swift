637a12b7c55c278b8ba63bf693f826d6578312ce
Rafael Escaleira <rafaelescaleira@icloud.com>
2023-09-19 17:32:33 -0400

    Create URL+Extensions.swift

---
+++ Sources/RefdsCore/Domain/Helpers/URL+Extensions.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public extension URL {
+    static func storeURL(for appGroup: String, databaseName: String) -> URL {
+        guard let fileContainer = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroup) else {
+            fatalError("Shared file container could not be created.")
+        }
+        return fileContainer.appendingPathComponent("\(databaseName).sqlite")
+    }
+}
